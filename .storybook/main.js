b335c9f3dfa65d5430850f8847f904a77f22d623
jfernandezpe <jfernandezpe.dev@gmail.com>
2021-01-25 17:58:49 +0100

    fix: solve storybook issue

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,23 @@
+/* 
+I'm using the legacy open-wc's storybook due to a issue on windows:
+https://github.com/modernweb-dev/web/issues/1229
+
+*/
+
 module.exports = {
-  stories: ['../stories/**/*.stories.{js,md,mdx}'],
+  stories: ['../**/stories/*.stories.{js,md,mdx}'],
+  addons: [
+    'storybook-prebuilt/addon-docs/register.js',
+    'storybook-prebuilt/addon-viewport/register.js',
+    'storybook-prebuilt/addon-a11y/register.js',
+    'storybook-prebuilt/addon-docs/register.js',
+    'storybook-prebuilt/addon-actions/register.js',
+  ],
+  esDevServer: {
+    // custom es-dev-server options
+    nodeResolve: true,
+    watch: true,
+    open: true,
+  },
+  //TODO: disable keyboard shortcuts
 };
